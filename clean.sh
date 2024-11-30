2d23868ca5ed1ed638b4523ccf348f15c9ffa558
Robert Kirkman <misternumberone@live.com>
2024-11-30 09:43:37 -0600

    mac porting test 2

--- clean.sh
+++ clean.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-rm -r termux* 2>/dev/null
+set -x
+rm -rf termux* 2>/dev/null
 rm *.apk 2>/dev/null
-if docker container ls | grep termux-package-builder >/dev/null
+if docker container ls | grep termux-generator-package-builder >/dev/null
 then
-    docker container stop termux-package-builder >/dev/null
+    docker container kill termux-generator-package-builder >/dev/null
 fi
-if docker container ls -a | grep termux-package-builder >/dev/null
+if docker container ls -a | grep termux-generator-package-builder >/dev/null
 then
-    docker container rm termux-package-builder >/dev/null
+    docker container rm termux-generator-package-builder >/dev/null
 fi
